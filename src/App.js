e544a92e53deddbaa2ea1e564844557bcd19fb63
Jeff <jeff@Jeffs-iMac.local>
2021-05-19 20:51:44 -0600

    updated everything

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, Link } from 'react-router-dom';
+import HomePage from '../src/components/HomePage';
+import AboutPage from '../src/components/AboutPage';
+import Projects from '../src/components/Projects';
+import NotFound from '../src/components/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <nav className='app-nav'>
+          <ul className='nav-list'>
+          <Link to='/' className='nav-link'><li className='nav-item'>Home</li></Link>
+          <Link to='/about' className='nav-link'><li className='nav-item'>About</li></Link>
+          <Link to='/projects' className='nav-link'><li className='nav-item'>Projects</li></Link>
+        </ul>
+      </nav>
+      <main>
+        <Switch>
+          <Route
+            exact
+            path={'/'}
+            component={HomePage}
+          />
+          <Route
+            exact
+            path={'/about'}
+            component={AboutPage}
+          />
+          <Route
+            exact
+            path={'/projects'}
+            component={Projects}
+          />
+          <Route component={NotFound} />
+        </Switch>
+      </main>
     </div>
   );
 }
